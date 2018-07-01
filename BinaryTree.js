5d18918befebabd14efaeae78cff6e8e54c40b6e
magicwangxuanqi <1794594886@qq.com>
2018-07-01 13:27:29 +0800

    二叉树删除的三种方式

--- BinaryTree.js
+++ BinaryTree.js
@@ -131,6 +131,50 @@ function BinaryTree(){
     this.search = function (key) {//key是查找的值
         return searchNode(root, key)
     }
+
+    var findMinNode = function(node){
+        if(node){
+            while (node && node.left !== null){
+                node = node.left;
+            }
+            return node;
+        }
+        return null;
+    }
+    var removeNode = function (node, key) {
+        if(node === null){//如果当前节点不存在
+            return null;//返回空
+        }
+        if(key < node.key){//如果当前节点存在，判断如果要删除的值是否小于该节点的值，则进入左子树，递归调用直到找不到左孩子
+            node.left = removeNode(node.left, key)
+        }else if(key > node.key){//如果要删除的值大于该节点的值，则进入右子树，递归调用，直到找不到右孩子
+            node.right = removeNode(node.right, key)
+        }else {//如果要删除的值和该节点的值相等
+            //1.如果是叶子节点
+            if(node.left === null && node.right === null){
+                //判断左右节点是不是都不存在，如果不存在则将该节点置成空，返回该节点
+                node = null;//将该节点置成空，相当于删除该节点
+                return node;
+            }
+            //2.如果只含有一个子树
+            if(node.left === null){//如果当前节点的左子树为空，则表明当前节点只有右子树
+                node = node.right;//把当前要删除的节点换成右孩子这个节点
+                return node;
+            }else if(node.right === null){//如果当前节点的右子树为空，则表明当前节点只有左子树
+                node = node.left;//把当前要删除的节点换成左孩子这个节点
+                return node;
+            }
+
+            //3.如果含有两个子树
+            var aux = findMinNode(node.right);//找到右子树最小值节点
+            node.key = aux.key;
+            node.right = removeNode(node.right, aux.key)//将最小节点从当前节点的右子树中进行删除
+        }
+    }
+    //删除某个节点
+    this.remove = function (key) {
+        root = removeNode(root, key);//首先对根节点进行判断
+    }
 }
 var nodes = [8, 3, 10, 1, 6, 14, 4, 7, 13];
 var binaryTree = new BinaryTree();
@@ -144,8 +188,11 @@ var callback = function (key) {
 };
 // binaryTree.inOrderTraverse(callback)
 // binaryTree.preOrderTraverse(callback)
-binaryTree.postOrderTraverse(callback)
-console.log('min node is: ' + binaryTree.min());
-console.log('max node is: ' + binaryTree.max());
-console.log(binaryTree.search(7) ? 'key 7 is found' : 'key 7 is not found');
-console.log(binaryTree.search(9) ? 'key 9 is found' : 'key 9 is not found');
+// binaryTree.postOrderTraverse(callback)
+// console.log('min node is: ' + binaryTree.min());
+// console.log('max node is: ' + binaryTree.max());
+// console.log(binaryTree.search(7) ? 'key 7 is found' : 'key 7 is not found');
+// console.log(binaryTree.search(9) ? 'key 9 is found' : 'key 9 is not found');
+binaryTree.remove(1);//1-叶子节点
+binaryTree.remove(10);//10-只含有一个子树
+binaryTree.remove(3);//3-含有两个子树
